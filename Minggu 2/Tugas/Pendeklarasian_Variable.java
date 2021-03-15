b6786a0897613d856ff069321f46abafcf2281bc
Ayunda12 <ayundakusumawardani12@gmail.com>
2021-03-15 20:06:51 +0700

    Create Pendeklarasian_Variable.java

---
+++ Minggu 2/Tugas/Pendeklarasian_Variable.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smt2minggu2;
+
+/**
+ *
+ * @author acer
+ */
+public class Pendeklarasian_Variable {
+    boolean gameOver=false;
+    int studets= 50,classes=3;
+    double sales_tax;
+    short number1;
+    
+    int 2beOrNot2be; // ini salah ,angka tidak boleh didepan
+    float price index;// ini salah,  setelah tipe data hanya ada satu nama,
+    //jika dua nama dihubungkan dengan tanda underscore “_”
+    double lastYear'sPrice; //ini salah,Nama Variabel tidak boleh mengandung simbol, kecuali tanda “$” dan underscore (_).
+    long class; //ini salah Keyword tidak dapat digunakan sebagai nama variabel
+    
+}
