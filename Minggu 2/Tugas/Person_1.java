e667ad822edf90fab79a803aa4dd652d1876aa99
Ayunda12 <ayundakusumawardani12@gmail.com>
2021-03-15 20:07:03 +0700

    Create Person_1.java

---
+++ Minggu 2/Tugas/Person_1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smt2minggu2;
+
+/**
+ *
+ * @author acer
+ */
+class Person_1 {
+
+    private String name;
+    private int age;
+
+    public Person_1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int newAge){
+        age = newAge;
+    }
+    
+}
