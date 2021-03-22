115f1a1c1433b7017f280239fba5b163c709c8ee
Ayunda12 <ayundakusumawardani12@gmail.com>
2021-03-22 16:24:06 +0700

    Create Tampil_nama.java

---
+++ Minggu 3/Tugas/Tampil_nama.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smt2minggu3;
+
+/**
+ *
+ * @author acer
+ */
+public class Tampil_nama {
+
+    public static void main(String[] args) {
+        String[] nama = {"Ayunda", "Kusuma", "Wardani"};
+        for (int a = 0; a < nama.length; a++) {
+            System.out.print(nama[a] + " ");
+        }
+    }
+}
