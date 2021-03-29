077277b62b713cad25be40ebd29e19039e6e28b0
Ayunda12 <ayundakusumawardani12@gmail.com>
2021-03-29 15:22:28 +0700

    Create statement_while.java

---
+++ Minggu 4/Latihan/statement_while.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smt2minggu4;
+
+/**
+ *
+ * @author acer
+ */
+public class statement_while {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            //blok kode yang akan diulang
+            System.out.println("Perulangan ke-" + i);
+            //increment nilai i
+            i++;
+        }
+    }
+
+}
