127c8a41e87968dfb3ea13705e33dc190a05ac5a
Ayunda12 <ayundakusumawardani12@gmail.com>
2021-04-22 22:20:13 +0700

    Create A.java

---
+++ Minggu 7/Latihan/Abstract/A.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abstract;
+
+/**
+ *
+ * @author acer
+ */
+public abstract class A {
+    abstract public void method2();// deklarasi method abstract
+    //metode konkrit yang memiliki implementasi
+
+    public void method1() {
+        System.out.println("Method konkrit dari class A");
+    }
+
+}
